1dff9c28f3b9a8137a70725fa363ca1e7e8b19fc
FILIP LECHOWICZ <f.lechowicz@gmail.com>
2021-05-12 00:09:21 +0200

    Initial commit

---
+++ src/test/java/com/flechow/webcalculator/WebcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flechow.webcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
